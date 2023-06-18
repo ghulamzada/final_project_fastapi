2a42adb92c51c8f0c7405bd1624807438a6057e2
ghulamzada <tajmohammad20@gmail.com>
2023-06-18 19:29:52 +0200

    updated unittests

---
+++ model/modeling.py
@@ -0,0 +1,75 @@
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.tree import DecisionTreeClassifier
+from joblib import dump
+
+
+# Optional: implement hyperparameter tuning.
+def train_model(X_train, y_train):
+    """
+    Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+    Returns
+    -------
+    model
+        Trained machine learning model.
+    """
+
+    # Building Decision Tree model 
+    trained_model_clf = DecisionTreeClassifier(random_state=42)
+    trained_model_clf = trained_model_clf.fit(X_train, y_train)
+    return trained_model_clf
+
+
+def compute_model_metrics(y, preds):
+    """
+    Validates the trained machine learning model using precision, recall, and F1.
+
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+    """
+    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
+    precision = precision_score(y, preds, zero_division=1)
+    recall = recall_score(y, preds, zero_division=1)
+    return precision, recall, fbeta
+
+
+def inference(model, X):
+    """ Run model inferences and return the predictions.
+
+    Inputs
+    ------
+    model : ???
+        Trained machine learning model.
+    X : np.array
+        Data used for prediction.
+    Returns
+    -------
+    preds : np.array
+        Predictions from the model.
+    """
+    y_pred = model.predict(X)
+    return y_pred
+
+def save_trained_model(trained_model_clf, path_to_save: str):
+    """
+    Save trained model
+
+    Input: trained model
+    Output: saved trained ml model in "joblib" format
+    """
+    dump(trained_model_clf, path_to_save)
---
+++ model/test_model.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import pytest
+from modeling import train_model, compute_model_metrics, inference
+from sklearn.model_selection import train_test_split
+from data import process_data
+
+@pytest.fixture(scope="session")
+def data():
+    data = pd.read_csv("testdataset_unittest.csv")
+    # Droping duplicate rows
+    data.drop_duplicates(inplace=True)
+    # Removing 2 unneeded columns
+    data.drop(['capital-gain', 'capital-loss'], axis=1, inplace=True)
+    # Optional enhancement, use K-fold cross validation instead of a train-test split.
+    train, test = train_test_split(data, test_size=0.20)
+    # Proces the test data with the process_data function.
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True)
+    
+    return X_train, y_train, cat_features, train, test, encoder, lb
+
+
+def test_train_model(data):
+    X_train, y_train, cat_features, train, test, encoder, lb = data
+    training = train_model(X_train=X_train, y_train=y_train)
+    assert str(training) == 'DecisionTreeClassifier(random_state=42)'
+
+    # Testing inference from model code
+    X_test, y_test, _, _ = process_data(
+        test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb)
+    y_pred = inference(training, X_test)
+    expected_pred = [0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0]
+    assert int(len(y_pred) == int(len(expected_pred)))
+
+
+    # Testing compute_model_metrics from model code
+    fbeta, precision, recall = compute_model_metrics(y_test, y_pred)
+
+    threshold = 0.5
+    assert precision < threshold
+    assert recall <= threshold
+    assert fbeta <= threshold
+
